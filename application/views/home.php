ee8b5b721f0daee36828993a7bdf7fbba925f630
duwilaja <apaaja.software@gmail.com>
2022-10-30 16:31:45 +0700

    home ganti lampu

--- application/views/home.php
+++ application/views/home.php
@@ -72,7 +72,7 @@ $this->load->view("_sidebar",$data);
 
           <div class="col-12 col-sm-6 col-md-3">
             <div class="info-box mb-3">
-              <span class="info-box-icon bg-success elevation-1"><i class="far fa-lightbulb"></i></span>
+              <span class="info-box-icon bg-success elevation-1"><i class="fas fa-lightbulb"></i></span>
 
               <div class="info-box-content">
                 <span class="info-box-text">ON</span>
@@ -85,7 +85,7 @@ $this->load->view("_sidebar",$data);
           <!-- /.col -->
 		  <div class="col-12 col-sm-6 col-md-3">
             <div class="info-box mb-3">
-              <span class="info-box-icon bg-danger elevation-1"><i class="fas fa-lightbulb"></i></span>
+              <span class="info-box-icon bg-danger elevation-1"><i class="far fa-lightbulb"></i></span>
 
               <div class="info-box-content">
                 <span class="info-box-text">OFF</span>
